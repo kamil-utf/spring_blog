cda0b4efcdfc6898acd336264b420f709de0c640
Kns <web.kns@gmx.com>
2017-03-05 17:02:57 +0100

    Basic security configuration

--- src/main/java/com/example/blog/config/JpaConfig.java
+++ src/main/java/com/example/blog/config/JpaConfig.java
@@ -2,7 +2,6 @@ package com.example.blog.config;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.PropertySource;
 import org.springframework.core.env.Environment;
@@ -14,7 +13,6 @@ import org.springframework.orm.jpa.vendor.HibernateJpaVendorAdapter;
 import org.springframework.transaction.PlatformTransactionManager;
 import org.springframework.transaction.annotation.EnableTransactionManagement;
 
-import javax.persistence.EntityManagerFactory;
 import javax.sql.DataSource;
 import java.util.Properties;
 
@@ -25,7 +23,7 @@ import java.util.Properties;
 public class JpaConfig {
 
     private static final String MODEL_LOCATION = "com.example.blog.model";
-    private Environment env;
+    private final Environment env;
 
     @Autowired
     public JpaConfig(Environment env) {
