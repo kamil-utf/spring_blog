38d60064843acfa52cba9098bd1c8ef16e320826
Kns <web.kns@gmx.com>
2017-03-11 19:42:20 +0100

    Define LoginController that handle login and register URL pattern (GET method)

---
+++ src/main/java/com/example/blog/controller/LoginController.java
@@ -0,0 +1,31 @@
+package com.example.blog.controller;
+
+import com.example.blog.model.User;
+import com.example.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+@Controller
+public class LoginController {
+
+    private final UserService userService;
+
+    @Autowired
+    public LoginController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping("/login")
+    public String showLoginForm() {
+        return "login";
+    }
+
+    @GetMapping("/register")
+    public ModelAndView showRegistrationForm() {
+        ModelAndView modelAndView = new ModelAndView("register");
+        modelAndView.addObject("user", new User());
+        return modelAndView;
+    }
+}
