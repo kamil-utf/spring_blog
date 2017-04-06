44ba02b1d969c81ab05479650f3efbfb399865ac
Kns <web.kns@gmx.com>
2017-04-06 21:12:51 +0200

    Improved list of users after change view technology

---
+++ src/main/java/com/example/blog/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.example.blog.controller;
+
+import com.example.blog.model.User;
+import com.example.blog.service.UserService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import java.util.List;
+
+@Controller
+@RequestMapping("/admin/users")
+public class UserController {
+
+    private final UserService userService;
+
+    @Autowired
+    public UserController(UserService userService) {
+        this.userService = userService;
+    }
+
+    @GetMapping
+    public String browseUsers(Model model) {
+        List<User> users = userService.findAll();
+        model.addAttribute("users", users);
+        return "user/browse";
+    }
+}
