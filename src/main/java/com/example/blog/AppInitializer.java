b397c03de0c9e0e8fd226cbecbc70ea5205fae6b
Kns <web.kns@gmx.com>
2017-04-06 22:00:23 +0200

    Added HiddenHttpMethodFilter to AppInitializer

--- src/main/java/com/example/blog/AppInitializer.java
+++ src/main/java/com/example/blog/AppInitializer.java
@@ -4,8 +4,10 @@ import org.springframework.web.WebApplicationInitializer;
 import org.springframework.web.context.ContextLoaderListener;
 import org.springframework.web.context.WebApplicationContext;
 import org.springframework.web.context.support.AnnotationConfigWebApplicationContext;
+import org.springframework.web.filter.HiddenHttpMethodFilter;
 import org.springframework.web.servlet.DispatcherServlet;
 
+import javax.servlet.FilterRegistration;
 import javax.servlet.ServletContext;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRegistration;
@@ -19,10 +21,15 @@ public class AppInitializer implements WebApplicationInitializer {
 	public void onStartup(ServletContext servletContext) throws ServletException {
 		WebApplicationContext webContext = getWebContext();
 		servletContext.addListener(new ContextLoaderListener(webContext));
-		
-		ServletRegistration.Dynamic dispatcher = servletContext.addServlet("dispatcherServlet", new DispatcherServlet(webContext));
+
+		ServletRegistration.Dynamic dispatcher =
+                servletContext.addServlet("dispatcherServlet", new DispatcherServlet(webContext));
 		dispatcher.setLoadOnStartup(1);
 		dispatcher.addMapping(MAPPING_URL);
+
+        FilterRegistration.Dynamic hiddenHttpMethodFilter
+                = servletContext.addFilter("hiddenHttpMethodFilter", HiddenHttpMethodFilter.class);
+	    hiddenHttpMethodFilter.addMappingForUrlPatterns(null, false, "/*");
 	}
 	
 	private AnnotationConfigWebApplicationContext getWebContext() {
