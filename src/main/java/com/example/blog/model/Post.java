208dba7fe7dd37e58586033a803e65b677c003a5
Kns <web.kns@gmx.com>
2017-04-18 18:25:09 +0200

    Polish post entity

--- src/main/java/com/example/blog/model/Post.java
+++ src/main/java/com/example/blog/model/Post.java
@@ -1,9 +1,15 @@
 package com.example.blog.model;
 
+import org.hibernate.validator.constraints.NotBlank;
 import org.springframework.data.annotation.CreatedBy;
+import org.springframework.data.annotation.CreatedDate;
+import org.springframework.data.annotation.LastModifiedDate;
 import org.springframework.data.jpa.domain.support.AuditingEntityListener;
 
 import javax.persistence.*;
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+import java.util.Date;
 
 @Entity
 @EntityListeners(AuditingEntityListener.class)
@@ -14,11 +20,25 @@ public class Post {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @NotNull
+    @Size(min = 5, max = 50)
     private String title;
+
+    @Lob
+    @NotBlank
+    private String content;
 
     @ManyToOne
     @CreatedBy
     private User author;
+
+    @CreatedDate
+    @Column(name = "created_at")
+    private Date createdDate;
+
+    @LastModifiedDate
+    @Column(name = "updated_at")
+    private Date lastModifiedDate;
 
     public Long getId() {
         return id;
@@ -36,11 +56,35 @@ public class Post {
         this.title = title;
     }
 
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
     public User getAuthor() {
         return author;
     }
 
     public void setAuthor(User author) {
         this.author = author;
+    }
+
+    public Date getCreatedDate() {
+        return createdDate;
+    }
+
+    public void setCreatedDate(Date createdDate) {
+        this.createdDate = createdDate;
+    }
+
+    public Date getLastModifiedDate() {
+        return lastModifiedDate;
+    }
+
+    public void setLastModifiedDate(Date lastModifiedDate) {
+        this.lastModifiedDate = lastModifiedDate;
     }
 }
